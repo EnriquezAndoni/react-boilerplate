18d98b7df01a37fd9875d319771db1041606d2b3
EnriquezAndoni <github@andonienriquez.com>
2017-12-13 23:20:16 +0100

    feat: Display new home sceen

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,13 +4,14 @@ import './Styles/App.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Welcome to React</h1>
+          <h2 className='App-subtitle'>This is a React Boilerplate created by</h2>
+          <a className='Github-link'
+            href="https://github.com/EnriquezAndoni/react-boilerplate"
+            target='_blank'>Andoni Enriquez</a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     )
   }
