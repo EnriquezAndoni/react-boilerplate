8009d2bc0af7704d267dd7264e6982d4e52c8f80
EnriquezAndoni <github@andonienriquez.com>
2017-12-14 16:45:58 +0100

    feat: Introduce jsLingui

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
+
+// Translation
+import { I18nProvider, Trans } from 'lingui-react'
+import { unpackCatalog } from 'lingui-i18n'
+import catalog from '../locale/es/messages'
 
 // Actions
 import StartupActions from '../Redux/StartupRedux'
@@ -20,9 +25,11 @@ class App extends Component {
     }
   }
 
+  /*
   static propTypes = {
     attemptStartup: PropTypes.func
   }
+  */
 
   componentDidMount() {
     this.setState({ configuration: {text: 'New Configuration'} })
@@ -36,16 +43,20 @@ class App extends Component {
     const { attemptStartup } = this.props
     const { configuration } = this.state
 
+    const dev = process.env.NODE_ENV !== 'production' ? require('lingui-i18n/dev') : undefined
+
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Welcome to React</h1>
-          <h2 className='App-subtitle'>This is a React Boilerplate created by</h2>
-          <a className='Github-link' target='_blank' rel='noopener noreferrer'
-             href="https://github.com/EnriquezAndoni/react-boilerplate">Andoni Enriquez</a>
-        </header>
-        <button onClick={() => attemptStartup(configuration)}>Check your console</button>
-      </div>
+      <I18nProvider language="es" catalogs={{ es: unpackCatalog(catalog) }} development={dev}>
+        <div className='App'>
+          <header className='App-header'>
+            <h1 className='App-title'><Trans id='Welcome'>Welcome to React</Trans></h1>
+            <h2 className='App-subtitle'>This is a React Boilerplate created by</h2>
+            <a className='Github-link' target='_blank' rel='noopener noreferrer'
+               href="https://github.com/EnriquezAndoni/react-boilerplate">Andoni Enriquez</a>
+          </header>
+          <button onClick={() => attemptStartup(configuration)}>Check your console</button>
+        </div>
+      </I18nProvider>
     )
   }
 }
